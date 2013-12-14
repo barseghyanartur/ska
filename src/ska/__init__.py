57a049c8c77e54d4683cdf095371aa9b74c89447
Artur Barseghyan <artur.barseghyan@gmail.com>
2013-12-14 01:46:10 +0100

    forgotten version in init

--- src/ska/__init__.py
+++ src/ska/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'ska'
-__version__ = '1.0'
-__build__ = 0x00000A
+__version__ = '1.1'
+__build__ = 0x00000B
 __author__ = 'Artur Barseghyan'
 __copyright__ = 'Copyright (c) 2013 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
