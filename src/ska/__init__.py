32d0ba780c8bf71695a6fab3d1515285bebf92e7
Artur Barseghyan <artur.barseghyan@gmail.com>
2014-02-28 23:15:45 +0100

    changed version

--- src/ska/__init__.py
+++ src/ska/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'ska'
-__version__ = '1.4.2'
-__build__ = 0x00000E
+__version__ = '1.4.3'
+__build__ = 0x00000F
 __author__ = 'Artur Barseghyan'
 __copyright__ = 'Copyright (c) 2013 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
