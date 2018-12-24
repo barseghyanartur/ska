4098db6391430b16f71f68b3fd87d3c407bbad76
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-12-25 00:25:04 +0100

    more

--- src/ska/contrib/django/ska/__init__.py
+++ src/ska/contrib/django/ska/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 
-# __title__ = 'ska.contrib.django.ska.apps'
-# __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-# __copyright__ = '2013-2018 Artur Barseghyan'
-# __license__ = 'GPL 2.0/LGPL 2.1'
-# __all__ = ('default_app_config',)
-#
-# default_app_config = 'ska.contrib.django.ska.apps.Config'
+__title__ = 'ska.contrib.django.ska.apps'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2013-2018 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('default_app_config',)
+
+default_app_config = 'ska.contrib.django.ska.apps.Config'
