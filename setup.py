f635da7eeff1eadeea0994fabb06ac54eda3a3ef
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-02-23 00:16:45 +0100

    setup version up 1.7.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ try:
 except:
     readme = ''
 
-version = '1.7.1'
+version = '1.7.2'
 
 exec_dirs = [
     'src/ska/bin/',
@@ -68,5 +68,7 @@ setup(
         'pytest-django',
         'radar',
         'mock',
+        'beautifulsoup4',
+        'soupsieve',
     ]
 )
