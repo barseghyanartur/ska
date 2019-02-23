86cf740a4731e1b3c9d1ef6cceab42720d3ddc58
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-02-23 22:52:42 +0100

    better place for coverage commands

--- runtests.py
+++ runtests.py
@@ -2,9 +2,11 @@
 import os
 import sys
 import pytest
+import coverage
 
 
 def main():
+    coverage.process_startup()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.testing")
     sys.path.insert(0, os.path.abspath('src'))
     sys.path.insert(0, "examples/simple")
