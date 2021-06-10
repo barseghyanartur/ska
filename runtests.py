cc8b09175298c0a18d02be1a7162d6a2bfe40753
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-06-10 23:22:59 +0200

    Cleanup

--- runtests.py
+++ runtests.py
@@ -8,10 +8,10 @@ import coverage
 def main():
     coverage.process_startup()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.testing")
-    sys.path.insert(0, os.path.abspath('src'))
+    sys.path.insert(0, os.path.abspath("src"))
     sys.path.insert(0, "examples/simple")
     return pytest.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
