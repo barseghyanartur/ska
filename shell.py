c20cda51502eed2d8c3fb98f40a3f803c76206c9
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-06-10 23:24:30 +0200

    Clean up

--- shell.py
+++ shell.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
-import pytest
 
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.testing")
-    sys.path.insert(0, os.path.abspath('src'))
+    sys.path.insert(0, os.path.abspath("src"))
     sys.path.insert(0, "examples/simple")
     from IPython import start_ipython
+
     start_ipython(argv=[])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
