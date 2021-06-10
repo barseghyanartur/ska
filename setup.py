e44f75acf1fb599480eefda3acc13a03bc9494bd
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-06-10 22:02:15 +0200

    Blackify setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup, find_packages
 
 try:
-    readme = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+    readme = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 except:
-    readme = ''
+    readme = ""
 
-version = '1.8'
+version = "1.8"
 
 exec_dirs = [
-    'src/ska/bin/',
+    "src/ska/bin/",
 ]
 
 execs = []
@@ -17,10 +17,10 @@ for exec_dir in exec_dirs:
     execs += [os.path.join(exec_dir, f) for f in os.listdir(exec_dir)]
 
 setup(
-    name='ska',
+    name="ska",
     version=version,
     description="Sign- and validate- data (dictionaries, strings) using "
-                "symmetric-key algorithm.",
+    "symmetric-key algorithm.",
     long_description=readme,
     classifiers=[
         "Programming Language :: Python",
@@ -38,45 +38,44 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v2 or "
         "later (LGPLv2+)",
     ],
-    keywords='sign data, sign (HTTP) request, symmetric-key algorithm '
-             'encryption, sign URL, python, django, password-less login '
-             'django, password-less authentication backend django',
-    author='Artur Barseghyan',
-    author_email='artur.barseghyan@gmail.com',
-    url='https://github.com/barseghyanartur/ska',
-    package_dir={'': 'src'},
-    packages=find_packages(where='./src'),
+    keywords="sign data, sign (HTTP) request, symmetric-key algorithm "
+    "encryption, sign URL, python, django, password-less login "
+    "django, password-less authentication backend django",
+    author="Artur Barseghyan",
+    author_email="artur.barseghyan@gmail.com",
+    url="https://github.com/barseghyanartur/ska",
+    package_dir={"": "src"},
+    packages=find_packages(where="./src"),
     include_package_data=True,
     package_data={
-        'ska': execs,
+        "ska": execs,
     },
     project_urls={
         "Bug Tracker": "https://github.com/barseghyanartur/ska/issues",
         "Documentation": "https://ska.readthedocs.io/",
         "Source Code": "https://github.com/barseghyanartur/ska",
-        "Changelog": "https://ska.readthedocs.io/"
-                     "en/latest/changelog.html",
+        "Changelog": "https://ska.readthedocs.io/" "en/latest/changelog.html",
     },
-    license='GPL-2.0-only OR LGPL-2.1-or-later',
-    scripts=['src/ska/bin/ska-sign-url'],
+    license="GPL-2.0-only OR LGPL-2.1-or-later",
+    scripts=["src/ska/bin/ska-sign-url"],
     install_requires=[
-        'six>=1.9',
-        'django-nine>=0.2.4',
+        "six>=1.9",
+        "django-nine>=0.2.4",
     ],
     extras_require={
-        'django-constance': ['django-constance'],
-        'djangorestframework': ['djangorestframework'],
-        'drf-jwt': ['drf-jwt'],
+        "django-constance": ["django-constance"],
+        "djangorestframework": ["djangorestframework"],
+        "drf-jwt": ["drf-jwt"],
     },
     tests_require=[
-        'factory_boy',
-        'faker',
-        'pytest',
-        'pytest-django',
-        'radar',
-        'mock',
-        'beautifulsoup4',
-        'soupsieve',
-        'coverage',
-    ]
+        "factory_boy",
+        "faker",
+        "pytest",
+        "pytest-django",
+        "radar",
+        "mock",
+        "beautifulsoup4",
+        "soupsieve",
+        "coverage",
+    ],
 )
