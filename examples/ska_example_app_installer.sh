4c3bd3e0d9f4d3a2edee6117eb4d08bd16e45b58
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-08-27 01:08:05 +0200

    Clean up

--- examples/ska_example_app_installer.sh
+++ examples/ska_example_app_installer.sh
@@ -1,12 +1,13 @@
-wget -O ska_example_app_installer.tar.gz https://github.com/barseghyanartur/ska/archive/stable.tar.gz
+wget -O ska_example_app_installer.tar.gz https://github.com/barseghyanartur/ska/archive/main.tar.gz
 virtualenv ska
 source ska/bin/activate
 mkdir ska_example_app_installer/
 tar -xvf ska_example_app_installer.tar.gz -C ska_example_app_installer
 cd ska_example_app_installer/ska-stable/examples/simple/
 #cd ska_example_app_installer/ska-master/examples/simple/
-pip install -r ../../src/ska/contrib/django/ska/requirements.txt
-pip install https://github.com/barseghyanartur/ska/archive/stable.tar.gz
+#pip install -r ../../src/ska/contrib/django/ska/requirements.txt
+pip install -r ../../examples/requirements/django_4_2.txt
+pip install https://github.com/barseghyanartur/ska/archive/main.tar.gz
 mkdir ../media/
 mkdir ../media/static/
 mkdir ../static/
