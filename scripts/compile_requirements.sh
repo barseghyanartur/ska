ae291278b21ac759294ab642f26cd60bf615454c
Artur Barseghyan <artur.barseghyan@gmail.com>
2025-05-28 01:15:26 +0200

    More

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -2,46 +2,37 @@
 cd examples/requirements/
 
 echo "pip-compile common.in"
-pip-compile common.in "$@"
+uv pip compile ../../pyproject.toml common.in --all-extras -o common.txt "$@"
 
 echo "pip-compile debug.in"
-pip-compile debug.in "$@"
+uv pip compile ../../pyproject.toml debug.in --all-extras -o debug.txt "$@"
 
 echo "pip-compile deployment.in"
-pip-compile deployment.in "$@"
+uv pip compile ../../pyproject.toml deployment.in --all-extras -o deployment.txt "$@"
 
-echo "pip-compile deployment.in"
-pip-compile dev.in "$@"
-
-#echo "pip-compile django_2_2.in"
-#pip-compile django_2_2.in "$@"
-#
-#echo "pip-compile django_3_0.in"
-#pip-compile django_3_0.in "$@"
-#
-#echo "pip-compile django_3_1.in"
-#pip-compile django_3_1.in "$@"
+echo "pip-compile dev.in"
+uv pip compile ../../pyproject.toml dev.in --all-extras -o dev.txt "$@"
 
 echo "pip-compile django_3_2.in"
-pip-compile django_3_2.in "$@"
+uv pip compile ../../pyproject.toml django_3_2.in --all-extras -o django_3_2.txt "$@"
 
 echo "pip-compile django_4_1.in"
-pip-compile django_4_1.in "$@"
+uv pip compile ../../pyproject.toml django_4_1.in --all-extras -o django_4_1.txt "$@"
 
 echo "pip-compile django_4_2.in"
-pip-compile django_4_2.in "$@"
+uv pip compile ../../pyproject.toml django_4_2.in --all-extras -o django_4_2.txt "$@"
 
 echo "pip-compile docs.in"
-pip-compile docs.in "$@"
+uv pip compile ../../pyproject.toml docs.in --all-extras -o docs.txt "$@"
 
 echo "pip-compile documentation.in"
-pip-compile documentation.in "$@"
+uv pip compile ../../pyproject.toml documentation.in --all-extras -o documentation.txt "$@"
 
 echo "pip-compile style_checkers.in"
-pip-compile style_checkers.in "$@"
+uv pip compile ../../pyproject.toml style_checkers.in --all-extras -o style_checkers.txt "$@"
 
 echo "pip-compile test.in"
-pip-compile test.in "$@"
+uv pip compile ../../pyproject.toml test.in --all-extras -o test.txt "$@"
 
 echo "pip-compile testing.in"
-pip-compile testing.in "$@"
+uv pip compile ../../pyproject.toml testing.in --all-extras -o testing.txt "$@"
--- scripts/upgrade_requirements.sh
+++ scripts/upgrade_requirements.sh
@@ -1,14 +1,38 @@
 #!/usr/bin/env bash
 cd examples/requirements/
-pip-compile common.in --upgrade
-pip-compile debug.in --upgrade
-pip-compile deployment.in --upgrade
-pip-compile dev.in --upgrade
-pip-compile django_2_2.in --upgrade
-pip-compile django_3_0.in --upgrade
-pip-compile django_3_1.in --upgrade
-pip-compile django_3_2.in --upgrade
-pip-compile docs.in --upgrade
-pip-compile style_checkers.in --upgrade
-pip-compile test.in --upgrade
-pip-compile testing.in --upgrade
+
+echo "pip-compile common.in"
+uv pip compile ../../pyproject.toml common.in --all-extras -o common.txt --upgrade "$@"
+
+echo "pip-compile debug.in"
+uv pip compile ../../pyproject.toml debug.in --all-extras -o debug.txt --upgrade "$@"
+
+echo "pip-compile deployment.in"
+uv pip compile ../../pyproject.toml deployment.in --all-extras -o deployment.txt --upgrade "$@"
+
+echo "pip-compile dev.in"
+uv pip compile ../../pyproject.toml dev.in --all-extras -o dev.txt --upgrade "$@"
+
+echo "pip-compile django_3_2.in"
+uv pip compile ../../pyproject.toml django_3_2.in --all-extras -o django_3_2.txt --upgrade "$@"
+
+echo "pip-compile django_4_1.in"
+uv pip compile ../../pyproject.toml django_4_1.in --all-extras -o django_4_1.txt --upgrade "$@"
+
+echo "pip-compile django_4_2.in"
+uv pip compile ../../pyproject.toml django_4_2.in --all-extras -o django_4_2.txt --upgrade "$@"
+
+echo "pip-compile docs.in"
+uv pip compile ../../pyproject.toml docs.in --all-extras -o docs.txt --upgrade "$@"
+
+echo "pip-compile documentation.in"
+uv pip compile ../../pyproject.toml documentation.in --all-extras -o documentation.txt --upgrade "$@"
+
+echo "pip-compile style_checkers.in"
+uv pip compile ../../pyproject.toml style_checkers.in --all-extras -o style_checkers.txt --upgrade "$@"
+
+echo "pip-compile test.in"
+uv pip compile ../../pyproject.toml test.in --all-extras -o test.txt --upgrade "$@"
+
+echo "pip-compile testing.in"
+uv pip compile ../../pyproject.toml testing.in --all-extras -o testing.txt --upgrade "$@"
